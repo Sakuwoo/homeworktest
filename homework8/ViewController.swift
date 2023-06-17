4c10c1c1a1385670543ada5726980c0ae368cff7
Sakuwoo <kahorovasivara@gmail.com>
2023-06-17 19:03:28 +0600

    Initial Commit

---
+++ homework8/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  homework8
+//
+//  Created by Sevara on 17/6/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
